1227f561793963a724e39ac67cbf404231e29b5e
jaimin25 <jaiminchovatia25123@gmail.com>
2024-04-05 17:21:10 +0530

    world time app

---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location = '';
+  String time = '';
+  String flag = '';
+  String url = '';
+  late bool isDayTime;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    try {
+      http.Response response = await http
+          .get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+      Map data = jsonDecode(response.body);
+      // print(data);
+
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+
+      // print(datetime);
+      // print(offset);
+
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('caught error $e');
+      time = 'could not get time';
+    }
+  }
+}
