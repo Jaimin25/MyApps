7a831ea8760e2250f9ee2359f2cc9cd4f02dc937
jaimin25 <jaiminchovatia25123@gmail.com>
2024-04-08 16:43:21 +0530

    fix: removed unwanted import

--- todo_app/lib/pages/update_task.dart
+++ todo_app/lib/pages/update_task.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:toast/toast.dart';
 import 'package:todo_app/components/AppBar/update_task_app_bar.dart';
